2ddb422470ce3853b08135d4b05f74a007820c01
Max Khorsun <max.khorsun@gmail.com>
2020-06-25 20:11:02 +0300

    feat: main game screen

--- src/script.js
+++ src/script.js
@@ -31,3 +31,55 @@ const showGameLoadScreen = () => {
 };
 
 showGameLoadScreen();
+
+const showGameMainScreen = () => {
+  document.body.classList.remove('loading-screen');
+
+  app.innerHTML = '';
+
+  const wrapper = document.createElement('div');
+  wrapper.classList.add('wrapper');
+  app.append(wrapper);
+
+  const card = document.createElement('div');
+  card.classList.add('card');
+  wrapper.append(card);
+
+  const score = document.createElement('div');
+  score.classList.add('score');
+  card.append(score);
+
+  const question = document.createElement('div');
+  question.classList.add('question');
+  card.append(question);
+
+  const answer = document.createElement('div');
+  answer.classList.add('answer');
+  card.append(answer);
+
+  const buttonsContainer = document.createElement('div');
+  buttonsContainer.classList.add('buttons');
+  card.append(buttonsContainer);
+
+  const buttonAgree = document.createElement('button');
+  buttonAgree.classList.add('button');
+  buttonAgree.classList.add('agree');
+  buttonAgree.innerText = 'true';
+  buttonsContainer.append(buttonAgree);
+
+  const buttonDisagree = document.createElement('button');
+  buttonDisagree.classList.add('button');
+  buttonDisagree.classList.add('disagree');
+  buttonDisagree.innerText = 'false';
+  buttonsContainer.append(buttonDisagree);
+
+  const timer = document.createElement('div');
+  timer.classList.add('timer');
+  card.append(timer);
+};
+
+app.addEventListener('click', (event) => {
+  if (event.target.classList.contains('game-start-button')) {
+    showGameMainScreen();
+  }
+});
