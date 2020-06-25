7a9d3563d4750b814cabdf04eca280e05326a16b
Max Khorsun <max.khorsun@gmail.com>
2020-06-25 19:23:19 +0300

    feat: game loading screen

--- src/script.js
+++ src/script.js
@@ -1,3 +1,33 @@
 import './style.css';
 
-console.log('Hello world!');
+const app = document.querySelector('.app');
+
+const showGameLoadScreen = () => {
+  document.body.classList.add('loading-screen');
+
+  const wrapper = document.createElement('div');
+  wrapper.classList.add('wrapper');
+  app.append(wrapper);
+
+  const gameStartScreen = document.createElement('div');
+  gameStartScreen.classList.add('game-start-screen');
+  wrapper.append(gameStartScreen);
+
+  const gameTitle = document.createElement('h2');
+  gameTitle.classList.add('game-title');
+  gameTitle.innerText = 'Sprint';
+  gameStartScreen.append(gameTitle);
+
+  const gameDescription = document.createElement('p');
+  gameDescription.classList.add('game-description');
+  gameDescription.innerText = 'Test your knowledge by answering true or false. You have 60 seconds to do this.';
+  gameStartScreen.append(gameDescription);
+
+  const gameStartButton = document.createElement('button');
+  gameStartButton.classList.add('button');
+  gameStartButton.classList.add('game-start-button');
+  gameStartButton.innerText = 'Start';
+  gameStartScreen.append(gameStartButton);
+};
+
+showGameLoadScreen();
